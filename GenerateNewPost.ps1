d2192aa93b7eb360a330e940649d023777ab4cb3
ojikeii <ojikeii@gmail.com>
2019-08-10 20:25:16 +0900

    Omit variables of Title and Tags.

--- GenerateNewPost.ps1
+++ GenerateNewPost.ps1
@@ -2,9 +2,9 @@ $name = $(Get-Date -Format "yyyy-MM-dd-HHmmss")
 $path = Join-Path $PSScriptRoot "input/posts/${name}.md"
 
 $content = @"
-Title: ${Title}
+Title: 
 Published: $(Get-Date -Format "MM/dd/yyyy")
-Tags: ${tagText}
+Tags: 
 ---
 "@
 
