ab298b75fef9c819a6a108f5c49f440516ca78f1
ojikeii <ojikeii@gmail.com>
2020-01-14 01:19:33 +0900

    feat: update the generate post script for cooking tag

--- GenerateNewPost.ps1
+++ GenerateNewPost.ps1
@@ -1,11 +1,31 @@
+Param($tag)
+
 $name = $(Get-Date -Format "yyyy-MM-dd-HHmmss")
 $path = Join-Path $PSScriptRoot "input/posts/${name}.md"
+
+$template = ""
+switch -Exact ($tag) {
+    "cooking" {
+        $template=@"
+# 動機
+# 材料
+|材料|量|
+|---|---|
+# 手順
+# 感想
+"@
+      }
+    Default {}
+}
 
 $content = @"
 Title: 
 Published: $(Get-Date -Format "MM/dd/yyyy")
-Tags: 
+Tags: $tag
 ---
+$template
 "@
+
+
 
 Set-Content -Path $path -Value $content
