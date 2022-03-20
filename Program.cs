78c223fecf5d6c88f6562550b69889e8c42df126
ojikeii <ojikeii@gmail.com>
2022-03-20 22:34:56 +0900

    chore: use github action

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace ojikeii.github.io
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .RunAsync();
+    }
+}
